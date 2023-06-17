bf6bf978bd03abf5bfe928e4cee3393083ca1639
Miguel Ángel Rodríguez García <ma.rodgar1999@gmail.com>
2023-06-17 16:42:55 +0200

    Create SBR-FC.cpp

---
+++ SBR-FC.cpp
@@ -0,0 +1,28 @@
+#include "stdlib.h"
+#include "iostream"
+
+using namespace std;
+
+class regla
+{
+private:
+    /* data */
+public:
+    regla(/* args */);
+    ~regla();
+};
+
+regla::regla(/* args */)
+{
+}
+
+regla::~regla()
+{
+}
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
